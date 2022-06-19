c100ea2176e089fbc2f672522a295b07ad6f129a
CleverBe <Cleverbernal123@gmail.com>
2022-06-19 16:24:32 -0400

    agregado usuarios login

---
+++ ApiCompetencia/Models/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCompetencia.Models
+{
+    [Table("usuarios")]
+    public class Usuarios
+    {
+
+        [Key]
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
